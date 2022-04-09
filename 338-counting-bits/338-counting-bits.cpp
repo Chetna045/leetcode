fd7884c61930e5af84a55457d20f239721266f5b
Chetna045 <89128684+Chetna045@users.noreply.github.com>
2022-04-09 23:58:24 +0530

    Time: 11 ms (29.47%), Space: 7.9 MB (74.29%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>ans;
-        for(int i=0;i<=n;i++)
+        vector<int>ans(n+1);
+       /* for(int i=0;i<=n;i++)
         { int sum=0;
             int num=i;
             while(num>0)
@@ -12,7 +12,14 @@ public:
                 
             }
          ans.push_back(sum);
+        }*/
+        ans[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+            ans[i]=ans[i/2]+i%2;
         }
+        
+        
         return ans;
     }
 };
