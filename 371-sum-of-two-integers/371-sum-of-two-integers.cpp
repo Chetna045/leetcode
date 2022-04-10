7a2ae5a8eb808de29614e09df4f0885dee8a2f8d
Chetna045 <89128684+Chetna045@users.noreply.github.com>
2022-04-10 15:31:45 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (27.67%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        if(a==0)
+        /*if(a==0)
             return b;
         if(b==0)
             return a;
-        return log(exp(a)*exp(b));
+        return log(exp(a)*exp(b));*/
+        while(b)
+        {
+            unsigned c=a&b;
+            a^=b;
+            b=c<<1;
+        }
+        return a;
     }
 };
