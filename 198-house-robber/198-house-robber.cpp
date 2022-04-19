97b9368e668d7d4fe1e0b838fbb4eef3924a583a
Chetna045 <89128684+Chetna045@users.noreply.github.com>
2022-04-19 17:51:45 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (76.18%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -8,8 +8,11 @@ public:
         return dp[i]=max(rob(nums,dp,i+1),rob(nums,dp,i+2)+nums[i]);
     }*/
     int rob(vector<int>& nums) {
-       /*vector<int> dp(nums.size(),-1);
+       /*//1st appr
+       vector<int> dp(nums.size(),-1);
         return rob(nums,dp,0);*/
+     
+        /* //second approach
         if(size(nums)==1)
             return nums[0];
         vector<int>dp(nums);
@@ -18,8 +21,18 @@ public:
         for(int i=2;i<size(nums);i++)
             dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
         
-        return dp[size(nums)-1];
-            
+        return dp[size(nums)-1];*/
+            //third approach
+        if(nums.size()==1)
+            return nums[0];
+        int prev=0;
+        int curr=0;
+        for(auto num :nums)
+        {int temp=curr;
+         curr=max(curr,prev+num);
+         prev=temp;
+         }
+        return curr;
     }
     
   
